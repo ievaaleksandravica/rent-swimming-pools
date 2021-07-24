b13411ebdb39ef87bb8b363fe6cf24796c649f5d
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-07-24 16:28:47 +0200

    creating main routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pools do
+    resources :bookings, only: [:create, :index]
+  end
+
 end
