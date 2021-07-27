2b690a8eb1a70432dc26b43b5af196777bab9fde
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-07-27 19:15:54 +0200

    create new pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -7,9 +7,27 @@ class PoolsController < ApplicationController
 
   def show; end
 
+  def new
+    @pool = Pool.new
+  end
+
+  def create
+    @pool = Pool.new(pool_params)
+    @pool.user = current_user
+    if @pool.save
+      redirect_to :pools
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_pool
-    @pool = Pool.find(params[:id]) 
+    @pool = Pool.find(params[:id])
+  end
+
+  def pool_params
+    params.require(:pool).permit(:title, :description, :street, :city, :zip_code, :category, :qm, :price_per_day, :max_capacity)
   end
 end
